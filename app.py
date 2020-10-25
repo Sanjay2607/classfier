f97ef3a36a66ece2e407dd758959558f1e1adb32
Sanjay Singh <48975899+Sanjay2607@users.noreply.github.com>
2020-10-25 22:48:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def load_img(path):
 
 learn = load_learner_('export.pkl')
 
-st.markdown("# Animal Classifier")
-st.markdown("Upload an image and the classifier will tell you whether its a horse, dog or bear.")
+st.markdown("# Mirzapur Characters")
+st.markdown("Upload an image and the classifier will tell you character.")
 
 file_bytes = st.file_uploader("Upload a file", type=("png", "jpg", "jpeg", "jfif"))
 if file_bytes:
